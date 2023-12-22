6edebe27c263e4a78e1d526cb163ecb4106ebecc
Thy Antonio <58723692+thyronea@users.noreply.github.com>
2023-12-21 17:04:39 -0800

    Add files via upload

---
+++ app/dir/private/components/scripts.php
@@ -0,0 +1,87 @@
+<!-- Autotab input on login verification code -->
+<script type="text/javascript">
+  $(".inputs").keyup(function () {
+      if (this.value.length == this.maxLength) {
+        $(this).next('.inputs').focus();
+      }
+  });
+</script>
+
+<!-- Script for generating random User ID -->
+<script>
+ function randomNumber(len) {
+  var randomNumber;
+  var n = '';
+
+  for (var count = 0; count < len; count++) {
+    randomNumber = Math.floor((Math.random() * 9) + 1);
+    n += randomNumber.toString();
+  }
+  return n;
+  }
+  document.getElementById("userID").value = randomNumber(7);
+</script>
+
+<!-- Script for generating random Group ID -->
+<script>
+ function randomNumber(len) {
+  var randomNumber;
+  var n = '';
+
+  for (var count = 0; count < len; count++) {
+    randomNumber = Math.floor((Math.random() * 9) + 1);
+    n += randomNumber.toString();
+  }
+  return n;
+  }
+  document.getElementById("groupID").value = randomNumber(7);
+</script>
+
+<!-- Script for generating random Office ID -->
+<script>
+ function randomNumber(len) {
+  var randomNumber;
+  var n = '';
+
+  for (var count = 0; count < len; count++) {
+    randomNumber = Math.floor((Math.random() * 9) + 1);
+    n += randomNumber.toString();
+  }
+  return n;
+  }
+  document.getElementById("officeID").value = randomNumber(7);
+</script>
+
+<!-- Script for generating random Engine ID -->
+<script>
+ function randomNumber(len) {
+  var randomNumber;
+  var n = '';
+
+  for (var count = 0; count < len; count++) {
+    randomNumber = Math.floor((Math.random() * 9) + 1);
+    n += randomNumber.toString();
+  }
+  return n;
+  }
+  document.getElementById("engineID").value = randomNumber(7);
+</script>
+
+<!-- Signup form password validation -->
+<script>
+ function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirm]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
+</script>
+
+<!-- Script for popover messages -->
+<script type="text/javascript">
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+</script>

5b5d1c6585457f4f67b9ca30fd8c54adee92812b
Thy Antonio <58723692+thyronea@users.noreply.github.com>
2023-12-27 12:12:17 -0800

    Update send-user-code-process.php

--- app/dir/private/security/send-user-code-process.php
+++ app/dir/private/security/send-user-code-process.php
@@ -46,12 +46,12 @@ if(isset($_POST['user_send_code']))
     $mail->isSMTP();
     $mail->SMTPAuth = true;
 
-    $mail->Host = "smtp.gmail.com";
+    $mail->Host = "";
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $mail->Port = ;
 
-    $mail->Username = "thyrone.antonio@gmail.com";
-    $mail->Password = "mhopftvkjlemevgn";
+    $mail->Username = "";
+    $mail->Password = "";
 
     $mail->setFrom($email);
     $mail->addAddress($email);
